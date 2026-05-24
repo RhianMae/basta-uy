225d8d2228735f8ed43d9371e6bb345561bc4b55
RhianMae <opalla.rhian@gmail.com>
2026-05-24 20:46:39 +0800

    Update basta uy 1.py

--- basta uy 1.py
+++ basta uy 1.py
@@ -6,12 +6,3 @@ try:
 except FileExistsError:
     print("Student file already exists!")
 
-def add_student():
-    student_id = input("Enter Student ID: ")
-    name = input("Enter Name: ")
-    course = input("Enter Course: ")
-
-    with open(FILENAME, "a") as file:
-        file.write(f"{student_id},{name},{course}\n")
-
-    print("Student added successfully!")
